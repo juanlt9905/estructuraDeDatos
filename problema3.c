e1bea4febff34034e458f706f2850261d467341b
juanlt9905 <juanlt9905@gmail.com>
2025-03-26 17:33:06 -0700

    problema3

---
+++ problema3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void inicializar_arreglo(int *arreglo, int n){
+    
+
+    for(int i=0; i<n; i++){
+        arreglo[i] = i;
+    }
+
+
+}
+
+int main(int argc, char **argv){
+
+    int n = atoi(argv[1]);
+    int arreglo[n];
+    inicializar_arreglo(&arreglo[4], n); // con el & indico que quiero apuntar a la posicion donde tengo guardado arreglo[0]
+
+
+    for(int i=0; i<n; i++){
+        printf("%d ", arreglo[i]);
+    }
+
+
+
+    return 0;
+}
