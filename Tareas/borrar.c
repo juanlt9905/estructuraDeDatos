efc540fa1eae719f957e9e60ec5e07b1126a6c9a
juanlt9905 <juanlt9905@gmail.com>
2025-04-04 15:10:46 -0700

    cifrado

---
+++ Tareas/borrar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(){
+
+    if('A'!='a')
+        printf("No Son iguales");
+
+
+    /*for (int i=0; i<=26; i++)
+        printf("%c ", 'A'+i);*/
+
+
+    return 0;
+}
--- Tareas/cifrado_por_desplazamiento.c
+++ Tareas/cifrado_por_desplazamiento.c
@@ -4,33 +4,37 @@
 char alfabeto[]="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 int main(int argc, char **argv){
-    int desplazamiento = atoi(argv[1]);
-    int posicion_alfabeto;
-    int longitud_cadena=0;
+    int desplazamiento = atoi(argv[1]); //Lectura del desplazamiento.
+    int longitud_cadena=0; //Variable que servira para medir la longitud de la cadena a cifrar.
 
+    //Conteo de la longitud de la cadena a cifrar.
     for(int i =0; argv[2][i]!='\0'; i++){
         longitud_cadena++;
     }
-    char cadena[longitud_cadena];
+    char cadena[longitud_cadena+1]; //cadena a cifrar.
+
 
     for(int i=0; argv[2][i]!='\0'; i++){
         cadena[i] = argv[2][i];
     }
+
+    cadena[longitud_cadena] = '\0'; //caracter nulo al final de la cadena.
     
 
     for(int i=0; cadena[i]!='\0'; i++){ // recorrer la cadena a cifrar
-        for (int j=0; alfabeto[j]!='\0'; j++){
-            if(cadena[i]==alfabeto[j]){
-                posicion_alfabeto=j;
-                break;
+        if(cadena[i]>='A' && cadena[i]<='Z'){ //solamente se realiza el cifrado con letras mayusculas.
+            for (int j=0; alfabeto[j]!='\0'; j++){
+                if(cadena[i]==alfabeto[j]){
+                    cadena[i] = alfabeto[(j + desplazamiento)%26];
+                    break;
+                }
             }
         }
-        cadena[i] = alfabeto[posicion_alfabeto+ desplazamiento];
     }
 
     for(int i=0; cadena[i]!='\0'; i++){
 
-        printf("%c\n", cadena[i]);
+        printf("%c", cadena[i]);
     }
 
 
