b0dd2dd98fc9ee58b521568ce7b5c574f085662c
juanlt9905 <juanlt9905@gmail.com>
2025-05-07 13:42:23 -0700

    ej5

--- tarea2/ej5.c
+++ tarea2/ej5.c
@@ -8,6 +8,12 @@ typedef struct{
     int nElementos;
 } Heap;
 
+void swap(int *x, int *y) {
+  int temp;
+  temp = *x;
+  *x = *y;
+  *y = temp;
+}
 
 void insertarElementoHeap(Heap * heap, int dato){
     if (heap->nElementos==MAX){
@@ -16,17 +22,14 @@ void insertarElementoHeap(Heap * heap, int dato){
     }
 
     heap->elementos[heap->nElementos++] = dato; //inserta el nuevo elemento al final.
-    int bandera=1, aux, j;
+    int aux, j;
     int k=heap->nElementos-1;
-    while(k>0 && bandera ==1){
-        bandera =0;//saldra del ciclo while cuando encuentre su posicion (su padre sea mayor)
-        if(heap->elementos[k]>heap->elementos[(k-1)/2]){
-            aux = heap->elementos[(k-1)/2];
-            heap->elementos[(k-1)/2] = heap->elementos[k];
-            heap->elementos[k]=aux;
-            k=(k-1)/2;
-            bandera=1;
-            }
+    while(k>0 && heap->elementos[k] > heap->elementos[(k-1)/2]){
+        //saldra del ciclo while cuando encuentre su posicion (su padre sea mayor)
+        aux = heap->elementos[(k-1)/2];
+        heap->elementos[(k-1)/2] = heap->elementos[k];
+        heap->elementos[k]=aux;
+        k=(k-1)/2;    
         }
 
 }
@@ -35,6 +38,39 @@ void inicializarHeap(Heap * heap){
     heap->nElementos=0;
 }
 
+void extraerMax(Heap * heap){
+    if (heap->nElementos == 0) {
+        printf("Cola vacia\n");
+        return; 
+    }
+
+    int k=0;
+
+    heap->elementos[0] = heap -> elementos[heap->nElementos-1];
+    heap->nElementos--;
+
+    int bandera=0;
+    while(bandera ==0){
+
+        int hijoIzq = 2*k+1; //indices de localizacion de los hijos 
+        int hijoDer = 2*k+2;
+        int mayor = k; //indice que almacenara el mayor elemento
+
+
+        if(hijoIzq < heap->nElementos && heap->elementos[hijoIzq] > heap->elementos[mayor])
+            mayor = hijoIzq;
+        if (hijoDer < heap->nElementos  && heap->elementos[hijoDer] > heap->elementos[mayor])
+            mayor=hijoDer;
+        if (mayor!=k){
+            swap(&heap->elementos[k], &heap->elementos[mayor]);
+            k=mayor;
+        }
+        else
+            bandera=1; //ya se encuentra en su posicion de la cola respetando el heap
+
+}       
+
+}
 
 
 
@@ -59,6 +95,14 @@ int main(){
     for (int i=0; i<heap.nElementos; i++){
         printf("%d ", heap.elementos[i]);
     }
+    printf("\n");
+    extraerMax(&heap);
+
+    for (int i=0; i<heap.nElementos; i++){
+        printf("%d ", heap.elementos[i]);
+    }    
+
+    printf("\n");
 
 
 
