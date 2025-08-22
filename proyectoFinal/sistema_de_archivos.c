7ac971ce7ad96d2c415758d7dd6a4b2a59e0d9ed
juanlt9905 <juanlt9905@gmail.com>
2025-08-22 10:11:39 -0700

    act

--- proyectoFinal/sistema_de_archivos.c
+++ proyectoFinal/sistema_de_archivos.c
@@ -69,8 +69,22 @@ void createEmptyVirtualDisk(int numberOfRecords){
 
     //Inicializar directorio raiz
     Bloque_directorio raiz;
-    raiz.contador_de_apuntadores=0;
+    raiz.contador_de_apuntadores=2;
+
+    //apuntador a el mismo "."
+    strcpy(raiz.apuntadores_de_objetos[0].nombre, ".");
+    raiz.apuntadores_de_objetos[0].esDirectorio = 1;
+    raiz.apuntadores_de_objetos[0].numero_de_bloque = 0;
+    raiz.apuntadores_de_objetos[0].size = 0;
+
+    // apuntador a su dir padre ".." (el mismo)
+    strcpy(raiz.apuntadores_de_objetos[1].nombre, "..");
+    raiz.apuntadores_de_objetos[1].esDirectorio = 1;
+    raiz.apuntadores_de_objetos[1].numero_de_bloque = 0;
+    raiz.apuntadores_de_objetos[1].size = 0;
+    
     WriteBlock(0, (char*)&raiz);
+
     bloques_libres[0]=0; //marcar raiz como ocupado.
 
     printf("Disco virtual de %d bloques de memoria.\n", numberOfRecords);
@@ -107,12 +121,17 @@ void liberar_bloque(int bloque){
 void mkdir(char *nombre_directorio){
 
     //Leemos el bloque actual para almacenar el puntero del nuevo directorio:
-    Bloque_directorio directorio_actual;
-    ReadBlock(bloque_actual, (char*)&directorio_actual); //almacenamos la info del bloque actual en directorio actual.
+    //Bloque_directorio directorio_actual;
+    //ReadBlock(bloque_actual, (char*)&directorio_actual); //almacenamos la info del bloque actual en directorio actual.
+
+    char buffer[BLOCK_SIZE];
+    ReadBlock(bloque_actual, buffer);
+
+    Bloque_directorio * directorio_actual = (Bloque_directorio*)buffer;
 
     //Verificar si ya existe el directorio
-    for(int i = 0; i < directorio_actual.contador_de_apuntadores; i++){
-        if(strcmp(directorio_actual.apuntadores_de_objetos[i].nombre, nombre_directorio) == 0){
+    for(int i = 0; i < directorio_actual->contador_de_apuntadores; i++){
+        if(strcmp(directorio_actual->apuntadores_de_objetos[i].nombre, nombre_directorio) == 0){
             printf("Error: '%s' ya existe.\n", nombre_directorio);
             return;
         }
@@ -126,17 +145,31 @@ void mkdir(char *nombre_directorio){
     }
 
     //copiar el apuntador del nuevo directorio
-    strcpy(directorio_actual.apuntadores_de_objetos[directorio_actual.contador_de_apuntadores].nombre, nombre_directorio);
-    directorio_actual.apuntadores_de_objetos[directorio_actual.contador_de_apuntadores].esDirectorio=1;
-    directorio_actual.apuntadores_de_objetos[directorio_actual.contador_de_apuntadores].numero_de_bloque=nuevo_bloque;
-    directorio_actual.apuntadores_de_objetos[directorio_actual.contador_de_apuntadores].size=0;
-    directorio_actual.contador_de_apuntadores++;
+    strcpy(directorio_actual->apuntadores_de_objetos[directorio_actual->contador_de_apuntadores].nombre, nombre_directorio);
+    directorio_actual->apuntadores_de_objetos[directorio_actual->contador_de_apuntadores].esDirectorio=1;
+    directorio_actual->apuntadores_de_objetos[directorio_actual->contador_de_apuntadores].numero_de_bloque=nuevo_bloque;
+    directorio_actual->apuntadores_de_objetos[directorio_actual->contador_de_apuntadores].size=0;
+    directorio_actual->contador_de_apuntadores++;
 
-    WriteBlock(bloque_actual, (char*)&directorio_actual);
+    WriteBlock(bloque_actual, buffer);
 
-    Bloque_directorio bloque;
-    bloque.contador_de_apuntadores=0;
-    WriteBlock(nuevo_bloque, (char*)&bloque);
+    Bloque_directorio nuevo_bloque_directorio;
+    
+    nuevo_bloque_directorio.contador_de_apuntadores=2; //inicia con dos entradas, "." y ".."
+    
+    //apuntador "." para apuntar al directorio actual
+    strcpy(nuevo_bloque_directorio.apuntadores_de_objetos[0].nombre, ".");
+    nuevo_bloque_directorio.apuntadores_de_objetos[0].esDirectorio=1;
+    nuevo_bloque_directorio.apuntadores_de_objetos[0].numero_de_bloque=nuevo_bloque;
+    nuevo_bloque_directorio.apuntadores_de_objetos[0].size = 0;
+
+    //apuntador ".." para apuntar al directorio padre
+    strcpy(nuevo_bloque_directorio.apuntadores_de_objetos[1].nombre, "..");
+    nuevo_bloque_directorio.apuntadores_de_objetos[1].esDirectorio=1;
+    nuevo_bloque_directorio.apuntadores_de_objetos[1].numero_de_bloque=bloque_actual;
+    nuevo_bloque_directorio.apuntadores_de_objetos[1].size = 0;
+
+    WriteBlock(nuevo_bloque, (char*)&nuevo_bloque_directorio);
     bloques_libres[nuevo_bloque]=0;
 
     printf("Directorio '%s' creado en bloque %d.\n", nombre_directorio, nuevo_bloque);
@@ -145,14 +178,15 @@ void mkdir(char *nombre_directorio){
 
 void ls(){
 
-     //Leemos el bloque actual para almacenar el puntero del nuevo directorio:
+     //Vaciamos el bloque actual en un buffer
     char buffer[BLOCK_SIZE];
     ReadBlock(bloque_actual, buffer);
 
+    //apuntador tipo Bloque_directorio a buffer
     Bloque_directorio* directorio_actual = (Bloque_directorio*)buffer;
 
     
-     //Bloque_directorio directorio_actual;
+     //Bloque_directorio directorio_actual;//no es el mismo espacio que BLOCK_SIZE ERROR
     //ReadBlock(bloque_actual, (char*)&directorio_actual);
 
     printf("Contenido del directorio: \n");
@@ -163,6 +197,66 @@ void ls(){
 
 }
 
+void cd(char * nombre_directorio){
+
+    if(strcmp(nombre_directorio, "/") == 0){
+        bloque_actual = 0;
+        printf("Cambiado al directorio raíz.\n");
+        return;
+    }
+
+    char buffer[BLOCK_SIZE];
+    ReadBlock(bloque_actual, buffer);
+    Bloque_directorio *directorio_actual = (Bloque_directorio*)buffer;
+
+    for (int i=0; i<directorio_actual->contador_de_apuntadores;i++){
+        if (strcmp(nombre_directorio, directorio_actual->apuntadores_de_objetos[i].nombre)==0 &&
+            directorio_actual->apuntadores_de_objetos[i].esDirectorio==1){
+                bloque_actual= directorio_actual->apuntadores_de_objetos[i].numero_de_bloque;
+                printf("Cambiado al directorio '%s' (bloque %d).\n", nombre_directorio, bloque_actual);
+                return;
+            }
+    }
+    printf("Error: Directorio '%s' no encontrado.\n", nombre_directorio);
+
+    
+
+}
+
+
+void obtener_nombre_dir_actual(char* nombre_directorio) {
+    // caso "/"
+    if (bloque_actual == 0) {
+        strcpy(nombre_directorio, "/");
+        return;
+    }
+
+    // Leer el bloque actual y encontrar el bloque del padre ("..")
+    char buffer_actual[BLOCK_SIZE];
+    ReadBlock(bloque_actual, buffer_actual);
+    Bloque_directorio* directorio_actual = (Bloque_directorio*)buffer_actual;
+
+    int numero_bloque_padre = -1;
+    for (int i = 0; i < directorio_actual->contador_de_apuntadores; i++) {
+        if (strcmp(directorio_actual->apuntadores_de_objetos[i].nombre, "..") == 0) {
+            numero_bloque_padre = directorio_actual->apuntadores_de_objetos[i].numero_de_bloque;
+            break;
+        }
+    }
+    // Leer el bloque del padre para encontrar nuestro propio nombre
+    char buffer_padre[BLOCK_SIZE];
+    ReadBlock(numero_bloque_padre, buffer_padre);
+    Bloque_directorio* directorio_padre = (Bloque_directorio*)buffer_padre;
+
+    for (int i = 0; i < directorio_padre->contador_de_apuntadores; i++) {
+        // Buscar el Apuntador que apunta a nuestro bloque actual
+        if (directorio_padre->apuntadores_de_objetos[i].numero_de_bloque == bloque_actual) {
+            strcpy(nombre_directorio, directorio_padre->apuntadores_de_objetos[i].nombre);
+            return;
+        }
+    }
+}
+
 int main(){
 
     
@@ -171,10 +265,15 @@ int main(){
 
     char comando[100];
     char arg1[50], arg2[50];
+    char nombre_directorio_actual[100];
+    
+
 
     while(1){
-        printf(">");
-        fgets(comando, 10, stdin);
+
+        obtener_nombre_dir_actual(nombre_directorio_actual);
+        printf("%s/>", nombre_directorio_actual);
+        fgets(comando, 50, stdin);
         comando[strcspn(comando,"\n")]= 0;
         // Limpiar argumentos
         memset(arg1, 0, sizeof(arg1));
@@ -208,6 +307,10 @@ int main(){
         }
         else if(strcmp(comando, "ls")==0){
             ls();
+        }
+        else if (strncmp(comando, "cd ", 3)==0){
+            sscanf(comando, "cd %s", arg1);
+            cd(arg1);
         }
 
     }
